b5c48fda070deadd2bd04f2370eda6482acdaf07
Jose Victor Hendz Martins <jose.victor@infoworld.com.br>
2018-09-18 08:36:44 -0300

    Add estrutura inicial

--- src/Controllers/test-controller.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export class TestController {
-  constructor() {}
-
-  public async getAll(request: Request, response: Response, next: NextFunction) {
-    response.json("test working");
-  }
-}
